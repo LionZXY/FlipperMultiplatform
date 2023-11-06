afe466dca725602411079d1cc5484f6062df1ed7
Nikita Kulikov <nikita@kulikof.ru>
2023-11-06 11:42:38 +0000

    Update kotlin and compose deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,12 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
-    kotlin("jvm")
-    id("org.jetbrains.compose")
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.compose.multiplatform)
 }
 
 group = "com.lionzxy"
 version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    google()
-}
 
 dependencies {
     // Note, if you develop a library, you should use compose.desktop.common.
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,11 +3,14 @@ pluginManagement {
         gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
-
-    plugins {
-        kotlin("jvm").version(extra["kotlin.version"] as String)
-        id("org.jetbrains.compose").version(extra["compose.version"] as String)
-    }
 }
 
+dependencyResolutionManagement {
+    repositories {
+        maven("https://jitpack.io")
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        mavenCentral()
+        google()
+    }
+}
 rootProject.name = "FlipperMultiplatform"
